35d2c1dae44a305ee0bc96534b55e806c2c6386f
Angga Hadi Permana <anggahadi447@gmail.com>
2025-07-22 23:15:15 +0800

    Integrate Laravel Fortify for authentication
    Added Laravel Fortify package and configuration for authentication, registration, password reset, profile updates, and two-factor authentication. Implemented related service provider, actions, migration for two-factor columns, and registration page with Inertia.js. Updated composer dependencies and providers.

---
+++ laravue/resources/js/layouts/GuestLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <section class="bg-light p-3 p-md-4 p-xl-5" style="height: 100vh">
+      <div class="container centered">
+        <div class="row justify-content-center">
+          <slot/>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.centered {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
